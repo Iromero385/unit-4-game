df6f14c331a2a258fff57e3683c966a9e7bb5cf5
Isaac Romarri <iromero385@berkeley.edu>
2018-08-06 09:10:55 -0700

    add

---
+++ assets/javascript/game.js
@@ -0,0 +1,74 @@
+var redColorValue = 0;
+var blueColorValue  = 0;
+var greenColorValue  = 0;
+var yellowColorValue  = 0;
+var randomNumber = 0;
+var numberOfWins = 0;
+var numberOfLosses = 0;
+var totalScore = 0;
+
+initializeNewGame();
+
+function initializeNewGame(){
+
+    redColorValue = Math.floor(Math.random()*100 +1);
+    blueColorValue = Math.floor(Math.random()*100 +1);
+    greenColorValue = Math.floor(Math.random()*100 +1);
+    yellowColorValue = Math.floor(Math.random()*100 +1);
+    randomNumber = Math.floor(Math.random()*1000 +1);
+    totalScore = 0;
+    $("#randNumber").text(randomNumber);
+}
+function endGame(){
+   $(".container").empty();
+   $(".container").html("<h1>" + "GameOver"+"</h1>")
+   $(".container").css("color", "red");
+   $(".container").css("text-align", "center");
+}
+function statusOfGame(){
+    if(randomNumber === totalScore){
+        numberOfWins++;
+        updateScreen();
+        initializeNewGame();
+        // code to end game play
+        // return true;
+    }
+    else if(totalScore < randomNumber){
+        // code to end game play
+        // return 1;
+    }
+    else{
+        numberOfLosses++;
+        console.log(numberOfLosses);
+        updateScreen();
+        initializeNewGame();
+        // code to end game play
+        // return false;
+    };
+}
+function updateScreen(){
+    $("#numberOfWins").text("Wins: " + numberOfWins);
+    $("#numberOfLosses").text("Losses: " + numberOfLosses);
+    $("#totalScore").text(totalScore);
+
+}
+$(".crystalImage").on("click",function(event){
+    // storing the image that was clicked on
+   var crystalColor = $(this).attr("data-color");
+   switch(crystalColor){
+       case "red":
+            totalScore= totalScore + redColorValue;
+            break;
+       case "blue":
+            totalScore= totalScore + blueColorValue;
+            break;
+       case "green":
+            totalScore= totalScore + greenColorValue;
+            break;
+       case "yellow":
+            totalScore= totalScore + yellowColorValue;
+            break;
+   }
+   statusOfGame();
+   updateScreen();
+});
